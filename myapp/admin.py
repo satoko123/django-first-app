dbac69245e8afa30870688c83fac991a5aaca1a0
satoko123 <moonysweets@gmail.com>
2021-09-21 22:42:34 +0900

    first commit

---
+++ myapp/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Post
+
+# 管理画面でPostモデルが表示されるようにする
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+  list_display = ('id', 'title', 'created_at')
+  list_display_links = ('title',) # 詳細に行くのにクリックする箇所
+  ordering = ('created_at',) # 並び順
+  
+
