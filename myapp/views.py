6fc5f5a43b2cdf6b26e4e469d30be3d1731eb4da
satoko123 <moonysweets@gmail.com>
2021-09-26 18:04:06 +0900

    投稿の個別ページをつ作る

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, CreateView, DetailView
 from .models import Post
 from django.urls import reverse_lazy
 from .forms import PostForm
@@ -21,4 +21,8 @@ class PostCreate(CreateView):
     model = Post
     form_class = PostForm
     success_url = reverse_lazy('myapp:index')
+
+
+class PostDetail(DetailView):
+    model = Post
   
