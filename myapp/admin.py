da801631cbaec5ce21087818bb757122eed122f0
satoko123 <moonysweets@gmail.com>
2021-10-10 18:29:46 +0900

    お気に入りに入れる機能を実装する

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Like
 
 # 管理画面でPostモデルが表示されるようにする
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
   list_display = ('id', 'author','title', 'created_at')
   list_display_links = ('title',) # 詳細に行くのにクリックする箇所
-  ordering = ('created_at',) # 並び順
+  ordering = ('-created_at',) # 並び順
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+  list_display = ('id', 'user', 'post')
+  list_display_links = ('post',) # 詳細に行くのにクリックする箇所
--- myapp/models.py
+++ myapp/models.py
@@ -11,3 +11,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title 
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, verbose_name="投稿", on_delete=models.PROTECT)
+    user = models.ForeignKey(User, verbose_name="お気に入りしたユーザー", on_delete=models.PROTECT)
