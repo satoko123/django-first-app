e7c740bc70388ff40fdd59ffa503a7f01fe70dc3
satoko123 <moonysweets@gmail.com>
2021-12-19 09:25:31 +0900

    404ページの設定

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&t-qwh%oy9@^r=*s932595xe!aum@4y=rk_#k_(*oeo0lh(agj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
