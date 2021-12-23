28d37e111a90d9df2a8e346a3ac8420458ac92df
satoko123 <moonysweets@gmail.com>
2021-12-23 11:58:29 +0900

    aws設定

--- myproject/settings.py
+++ myproject/settings.py
@@ -136,6 +136,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'myapp:login'
 LOGIN_REDIRECT_URL = 'myapp:index'
 
+AWS_STORAGE_BACKET_NAME = 'tsuna12345'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BACKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400', #一日はそのキャッシュを使用
+}
+
+
+
 AWS_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
